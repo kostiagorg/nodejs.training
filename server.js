175907f1abb19c35e72604a0bf99071c94db72a0
Yermalovich_K <Yermalovich_K@iba>
2017-05-04 18:48:31 +0300

    training: streams, ajax and json

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ http.createServer(function(request, response) {
    log(mname, "url : " + request.url);
    log(mname, "method : " + request.method);
    log(mname, "user-agent : " + request.headers["user-agent"]);
+   log(mname, "methods : " + request.headers["allow"]);
    log(mname, "headers:");
    log(mname, request.headers);   
 
@@ -21,10 +22,15 @@ http.createServer(function(request, response) {
    log(mname, "Write response ...");
 
    //determine if it is required to redirect to ./public/index.html
-   if (request.url.startsWith("/public")) {
+   //add replace option to use templates such {text} or other special combination
+   var prefix = 0;
+   if (request.url.startsWith("/public")) prefix = 1;
+   if (request.url.startsWith("/private")) prefix = 2;
+
+   if (prefix > 0) {
       log(mname, "Redirect to ./public/index.html");
       
-      fs.readFile("public/index.html", (error, data) => {
+      fs.readFile("public/index.html", "utf8", (error, data) => {
 
           if (error) {
               log(mname, "Can not read ./public/index.html");
@@ -32,20 +38,38 @@ http.createServer(function(request, response) {
               response.end("Resource NOT found");
           } else {
              //response.write("New CustomPac service!!!"); 
+             switch(prefix) {
+                case 1:
+                   data = data.replace("{text}", "This is public page!");   // <= can not di this because of "TypeError: data.replace is not a function",
+                                                                            // but if we open file in utf8 it works!!!
+                   break;
+                case 2:  
+                   data = data.replace("{text}", "This is private page!");
+                   break;
+                default:        
+                   data = data.replace("{text}", "This is default page!");
+             }
+
              response.end(data);
           }
-          //return;
       });
 
    } else {
-      //write default response
 
+      if (request.url == "/instruction") {
+         //try to use Streams and send *.doc file context -> works!
+         response.writeHead(200, {"Content-type" : "application/msword"});
+         fs.createReadStream("public/instruction.doc").pipe(response);
+         return;
+      } 
+
+      //write default response
       log(mname, "Default response");
       response.statusCode = 200;
       response.setHeader("Content-type", "text/html");
       response.setHeader("UserID", 194);
       response.write("<H1>Hello</H1>");
-      response.end("It is just training, but I have to train my brain everyday, every minute, every second!");
+      response.end("It is just training, but I have to train my brain everyday, every minute, every second!");     
    }   
    
    log(mname, "Write response ... OK!");
